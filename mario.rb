4e0866626fea4128f263d4ac629b7aec248f2707
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-27 20:57:46 +0000

    Automatically backed up by Learn

--- mario.rb
+++ mario.rb
@@ -1,10 +1,7 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  phrase == "It's-a me, Mario!"
 end
 
-phrase = "It's-a me, Mario!"
-
-def toadstool
-  puts status
-end
+mario 
